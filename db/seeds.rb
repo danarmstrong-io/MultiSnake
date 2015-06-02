8163e9c84b8494311b7b9c7ca85de0702a102c08
Daniel Armstrong <wtfdaboo@gmail.com>
2015-06-02 16:38:25 -0400

    working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ Snake.create(name: "mara", color: "pink", food: Food.create(name: "tardis"))
 Snake.create(name: "hydra", color: "blue", food: Food.create(name: "crustacean"))
 Snake.create(name: "unicorn", color: "purple", food: Food.create(name: "rainbow"))
 
-36.times do
-  Game.create
-end
+# 36.times do
+#   Game.create
+# end
 
 
 
@@ -21,18 +21,18 @@ end
 
 
 
-Game.all.each do |game|
-  snakes = Snake.all.shuffle
-  rand(5).times do
-    GameSnake.create(game: game, snake: snakes.pop, food: Food.all.sample, score: rand(40))
-  end
-  score = 0
-  game.game_snakes.all.each do |gs|
-    if gs.score > score
-      @winner = gs.snake
-    end
+# Game.all.each do |game|
+#   snakes = Snake.all.shuffle
+#   rand(5).times do
+#     GameSnake.create(game: game, snake: snakes.pop, food: Food.all.sample, score: rand(40))
+#   end
+#   score = 0
+#   game.game_snakes.all.each do |gs|
+#     if gs.score > score
+#       @winner = gs.snake
+#     end
 
-  end
-  game.snake_id = @winner.id
-  game.save
-end
+#   end
+#   game.snake_id = @winner.id
+#   game.save
+# end
